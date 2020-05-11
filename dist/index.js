7ca5d65f2e678b149b2b8395aed0fbf02998c20d
yuhongda <silverage.y@gmail.com>
2020-05-11 17:35:21 +0800

    Revert "parallel"
    This reverts commit 47842b8b1c92b03e24e243043acddb7be2a2d914.

--- dist/index.js
+++ dist/index.js
@@ -1,44 +1,21 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const webpack_sources_1 = require("webpack-sources");
-const esbuild_1 = require("esbuild");
-const p_limit_1 = __importDefault(require("p-limit"));
-let service;
 class ESBuildMinifierWebpackPlugin {
     apply(compiler) {
-        const pluginName = 'ESBuildMinifierWebpackPlugin';
-        const limit = p_limit_1.default(Infinity);
-        const queue = [];
-        compiler.hooks.emit.tapAsync(pluginName, async (compilation, callback) => {
+        compiler.hooks.emit.tapAsync('ESBuildMinifierWebpackPlugin', async (compilation, callback) => {
+            const esbuild = require('esbuild');
+            const service = await esbuild.startService();
             for (const key of Object.keys(compilation.assets)) {
                 if (/\.(js)$/.test(key)) {
-                    queue.push(limit(() => async () => {
-                        const source = compilation.assets[key].source();
-                        const { js } = await service.transform(source, { minify: true });
-                        compilation.assets[key] = new webpack_sources_1.RawSource(js || '');
-                    }));
+                    const source = compilation.assets[key].source();
+                    const { js } = await service.transform(source, { minify: true });
+                    // compilation.updateAsset(key, js);
+                    compilation.assets[key] = new webpack_sources_1.RawSource(js);
                 }
             }
-            try {
-                await Promise.all(queue);
-            }
-            catch (e) {
-                throw e;
-            }
+            service.stop();
             callback();
-        });
-        compiler.hooks.shouldEmit.tap(pluginName, async () => {
-            if (!service) {
-                service = await esbuild_1.startService();
-            }
-        });
-        compiler.hooks.afterEmit.tapAsync(pluginName, async () => {
-            if (service) {
-                await service.stop();
-            }
         });
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,24 @@
 import { Compiler, compilation } from 'webpack';
 import { RawSource } from 'webpack-sources';
-import { Service, startService } from 'esbuild';
-import pLimit, { Limit } from 'p-limit';
 
-let service: Service;
 class ESBuildMinifierWebpackPlugin {
   apply(compiler: Compiler) {
-    const pluginName = 'ESBuildMinifierWebpackPlugin';
-    const limit: Limit = pLimit(Infinity);
-    const queue: Array<Promise<() => Promise<void>>> = [];
-
     compiler.hooks.emit.tapAsync(
-      pluginName,
+      'ESBuildMinifierWebpackPlugin',
       async (compilation: compilation.Compilation, callback) => {
+        const esbuild = require('esbuild')
+        const service = await esbuild.startService()
+
         for (const key of Object.keys(compilation.assets)) {
           if (/\.(js)$/.test(key)) {
-            queue.push(limit(() => async () => {
-              const source: string = compilation.assets[key].source();
-              const { js } = await service.transform(source, { minify: true });
-              compilation.assets[key] = new RawSource(js || '');
-            }));
+            const source = compilation.assets[key].source();
+            const { js } = await service.transform(source, { minify: true });
+            // compilation.updateAsset(key, js);
+            compilation.assets[key] = new RawSource(js)
           }
         }
-
-        try{
-          await Promise.all(queue);
-        } catch (e) {
-          throw e;
-        }
+        service.stop();
         callback();
-      }
-    );
-
-    compiler.hooks.shouldEmit.tap(
-      pluginName, 
-      async () => {
-        if (!service) {
-          service = await startService()
-        }
-      }
-    );
-
-    compiler.hooks.afterEmit.tapAsync(
-      pluginName, 
-      async () => {
-        if (service) {
-          await service.stop();
-        }
       }
     );
   }
