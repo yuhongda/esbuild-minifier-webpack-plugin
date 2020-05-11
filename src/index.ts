47842b8b1c92b03e24e243043acddb7be2a2d914
yuhongda <silverage.y@gmail.com>
2020-05-11 17:15:45 +0800

    parallel

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,52 @@
 import { Compiler, compilation } from 'webpack';
 import { RawSource } from 'webpack-sources';
+import { Service, startService } from 'esbuild';
+import pLimit, { Limit } from 'p-limit';
 
+let service: Service;
 class ESBuildMinifierWebpackPlugin {
   apply(compiler: Compiler) {
-    compiler.hooks.emit.tapAsync(
-      'ESBuildMinifierWebpackPlugin',
-      async (compilation: compilation.Compilation, callback) => {
-        const esbuild = require('esbuild')
-        const service = await esbuild.startService()
+    const pluginName = 'ESBuildMinifierWebpackPlugin';
+    const limit: Limit = pLimit(Infinity);
+    const queue: Array<Promise<() => Promise<void>>> = [];
 
+    compiler.hooks.emit.tapAsync(
+      pluginName,
+      async (compilation: compilation.Compilation, callback) => {
         for (const key of Object.keys(compilation.assets)) {
           if (/\.(js)$/.test(key)) {
-            const source = compilation.assets[key].source();
-            const { js } = await service.transform(source, { minify: true });
-            // compilation.updateAsset(key, js);
-            compilation.assets[key] = new RawSource(js)
+            queue.push(limit(() => async () => {
+              const source: string = compilation.assets[key].source();
+              const { js } = await service.transform(source, { minify: true });
+              compilation.assets[key] = new RawSource(js || '');
+            }));
           }
         }
-        service.stop();
+
+        try{
+          await Promise.all(queue);
+        } catch (e) {
+          throw e;
+        }
         callback();
+      }
+    );
+
+    compiler.hooks.shouldEmit.tap(
+      pluginName, 
+      async () => {
+        if (!service) {
+          service = await startService()
+        }
+      }
+    );
+
+    compiler.hooks.afterEmit.tapAsync(
+      pluginName, 
+      async () => {
+        if (service) {
+          await service.stop();
+        }
       }
     );
   }
