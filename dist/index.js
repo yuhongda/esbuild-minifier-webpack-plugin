775ab76c680c597f4141e39296825323adf0ca01
yuhongda <silverage.y@gmail.com>
2020-05-14 00:29:15 +0800

    build

--- dist/index.js
+++ dist/index.js
@@ -1,43 +1,37 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const webpack_sources_1 = require("webpack-sources");
 const esbuild_1 = require("esbuild");
-const p_limit_1 = __importDefault(require("p-limit"));
-let service;
+let _service;
+const ensureService = async () => {
+    if (!_service) {
+        _service = await esbuild_1.startService();
+    }
+    return _service;
+};
 class ESBuildMinifierWebpackPlugin {
     apply(compiler) {
         const pluginName = 'ESBuildMinifierWebpackPlugin';
-        const limit = p_limit_1.default(Infinity);
-        const queue = [];
-        compiler.hooks.emit.tapAsync(pluginName, async (compilation, callback) => {
-            for (const key of Object.keys(compilation.assets)) {
-                if (/\.(js)$/.test(key)) {
-                    queue.push(limit(() => async () => {
-                        const source = compilation.assets[key].source();
-                        const { js } = await service.transform(source, { minify: true });
-                        compilation.assets[key] = new webpack_sources_1.RawSource(js || '');
-                    }));
+        compiler.hooks.compilation.tap(pluginName, (compilation) => {
+            compilation.hooks.optimizeChunkAssets.tapAsync(pluginName, async (chunks, callback) => {
+                const service = await ensureService();
+                for (const chunk of chunks) {
+                    for (const file of chunk.files) {
+                        if (/\.m?js(\?.*)?$/i.test(file)) {
+                            const source = compilation.assets[file].source();
+                            const { js } = await service.transform(source, { minify: true });
+                            compilation.assets[file] = new webpack_sources_1.RawSource(js || '');
+                        }
+                    }
+                    ;
                 }
-            }
-            try {
-                await Promise.all(queue);
-            }
-            catch (e) {
-                throw e;
-            }
-            callback();
-        });
-        compiler.hooks.shouldEmit.tap(pluginName, async () => {
-            if (!service) {
-                service = await esbuild_1.startService();
-            }
+                ;
+                callback();
+            });
         });
         compiler.hooks.afterEmit.tapAsync(pluginName, async () => {
-            if (service) {
-                await service.stop();
+            if (_service) {
+                await _service.stop();
             }
         });
     }
